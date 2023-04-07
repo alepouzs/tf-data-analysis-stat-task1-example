a2f725faf90cfb0e08ab2e28f3cb49ba2e3ed27f
Yulia Izmailova <104653247+alepouzs@users.noreply.github.com>
2023-04-08 00:57:53 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,4 +5,4 @@ import numpy as np
 chat_id = 487382438 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    return np.log(np.mean(x - 867)) - 1 / 2 * np.log(np.mean((x - 867) ** 2)/(np.mean(x - 867)) **2) # Ваш ответ
+    return np.log(x - 867).mean() # Ваш ответ
